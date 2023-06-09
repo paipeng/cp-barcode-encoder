d7f6b6df138469f2ab9e128b99b4bf60a730914f
Pai Peng <sipaipv6@gmail.com>
2023-06-09 12:10:05 +0800

    direct qr-writer with version

--- cpbarcode.cpp
+++ cpbarcode.cpp
@@ -5,10 +5,14 @@
 #include "BitMatrixIO.h"
 #include "CharacterSet.h"
 #include "MultiFormatWriter.h"
+#include "qrcode/QRWriter.h"
+#include "qrcode/QRErrorCorrectionLevel.h"
+
 #include <QDebug>
 #include <QErrorMessage>
 
 using namespace ZXing;
+using namespace QRCode;
 
 CPBarcode::CPBarcode()
 {
@@ -19,19 +23,24 @@ CPBarcode::CPBarcode()
 QImage CPBarcode::encode(const QString& data) {
     QImage barcode;
     try {
-        int width = 400, height = 400;
+        int width = 0, height = 0;
         BarcodeFormat format = BarcodeFormat::QRCode;
         int margin = 0;
-        CharacterSet encoding = CharacterSet::Unknown;
+        CharacterSet encoding = CharacterSet::GB18030;
         int eccLevel = 8;
 
-        auto writer = MultiFormatWriter(format).setMargin(margin).setEncoding(encoding).setEccLevel(eccLevel);
+        QRCode::ErrorCorrectionLevel ecc = QRCode::ErrorCorrectionLevel::High;
+
+        //auto writer = MultiFormatWriter(format).setMargin(margin).setEncoding(encoding).setEccLevel(eccLevel);
+
+        auto writer = QRCode::Writer().setEncoding(encoding).setMargin(margin).setErrorCorrectionLevel(ecc).setVersion(10);
+
 
         BitMatrix matrix = writer.encode(data.toStdWString(), width, height);
         //auto bitmap = ToMatrix<uint8_t>(matrix);
         qDebug() << "matrix size: " << matrix.width() << "-" << matrix.height();
 
-        barcode = QImage(width, height, QImage::Format_Grayscale8);
+        barcode = QImage(matrix.width(), matrix.height(), QImage::Format_Grayscale8);
         for (int i = 0; i < matrix.height(); i++) {
             for (int j = 0; j < matrix.width(); j++) {
                 if (matrix.get(j, i)) {
